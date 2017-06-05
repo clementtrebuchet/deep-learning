7d6ea05b122a8e1e0f6b4def959587725708ffe7
Clement Trebuchet <clement.trebuchet@gmail.com>
2017-06-05 18:20:34 +0200

    Done dlnd_tv_script_generation

---
+++ dlnd_tv_script_generation/command.sh
@@ -0,0 +1,4 @@
+pip install -r floyd_requirements.txt
+cp -r /code/* /output
+cd /output
+/run_jupyter.sh --no-browser --NotebookApp.base_url='/Quf7JknWPyjbyvc5tZPz5Y' --NotebookApp.token='' --NotebookApp.allow_origin='*'
