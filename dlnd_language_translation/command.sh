99e1012d230904db617bfa95d64592432fbf3f34
Clement Trebuchet <clement.trebuchet@gmail.com>
2017-07-03 18:57:45 +0200

    Finnish project dlnd_language_translation

---
+++ dlnd_language_translation/command.sh
@@ -0,0 +1,4 @@
+pip install -r floyd_requirements.txt
+cp -r /code/* /output
+cd /output
+/run_jupyter.sh --no-browser --NotebookApp.base_url='/ji27tmHDnht8VyVwCSdVV7' --NotebookApp.token='' --NotebookApp.allow_origin='*'
